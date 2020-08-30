9a825fd288732959d8d7c1a24ee60c8454d33fc1
DeanLaz <Dean@deansmac.lan>
2020-08-30 16:47:59 -0700

    Fixed API with Async / Added Button Event Listener

--- main.js
+++ main.js
@@ -1,11 +1,52 @@
 // SELECTORS
 
-const searchBtn = document.querySelector("#search-btn");
-const searchInput = document.querySelector("#search-input");
+const searchBtn = document.getElementById("search-btn");
+const searchInput = document.getElementById("search-input");
+const giphyDiv = document.getElementById("giphy-container");
 
-var xhr = $.get(
-  "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=F3hdmwTeh5VQtMeFRhODHQxNtUT5jzL1&limit=5"
-);
-xhr.done(function (data) {
-  console.log("success got data", data);
+const giphyKey = "F3hdmwTeh5VQtMeFRhODHQxNtUT5jzL1";
+
+async function main(searchKeyword) {
+  const URL = `http://api.giphy.com/v1/gifs/search?q=${searchKeyword}&api_key=${giphyKey}&limit=5`;
+  const fetchResult = fetch(URL);
+  const response = await fetchResult;
+  const jsonData = await response.json();
+  console.log(jsonData);
+  console.log(jsonData.data[0].images.original.url);
+  for (let i = 0; i < jsonData.data.length; i++) {
+    var giphyImage = document.createElement("img");
+    giphyImage.src = jsonData.data[i].images.original.url;
+    giphyImage.height = 100;
+    giphyImage.length = 100;
+    console.log(jsonData.data[i].images.original.url);
+    giphyDiv.appendChild(giphyImage);
+  }
+}
+searchBtn.addEventListener("click", function handleSearch() {
+  giphyDiv.innerHTML = "";
+  let searchKey = searchInput.value;
+  return main(searchKey);
 });
+
+// main("javascript");
+
+// var xhr = $.get(
+//   "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=F3hdmwTeh5VQtMeFRhODHQxNtUT5jzL1&limit=5"
+// );
+// xhr.done(function (data) {
+//   console.log("success got data", data);
+
+// });
+
+// function giphySearch(keyword) {
+//   fetch(
+//     `http://api.giphy.com/v1/gifs/search?q=dogs&api_key=${giphyKey}&limit=5`
+//   )
+//     .then((response) => response.json())
+//     .then((data) => console.log(data));
+// }
+
+// const result = data.data;
+// for (let i = 0; i < result.length; i++) {
+//   giphyDiv.innerHTML = result[i].images.original.url;
+// }
