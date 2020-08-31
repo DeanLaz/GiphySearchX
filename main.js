930e01de23c537aa0194dd4e767a97b17e1016de
DeanLaz <Dean@deansmac.lan>
2020-08-30 17:57:59 -0700

    Changed Fetch

--- main.js
+++ main.js
@@ -7,17 +7,16 @@ const giphyDiv = document.getElementById("giphy-container");
 const giphyKey = "F3hdmwTeh5VQtMeFRhODHQxNtUT5jzL1";
 
 async function main(searchKeyword) {
-  const URL = `http://api.giphy.com/v1/gifs/search?q=${searchKeyword}&api_key=${giphyKey}&limit=5`;
-  const fetchResult = fetch(URL);
-  const response = await fetchResult;
+  const URL = `http://api.giphy.com/v1/gifs/search?q=${searchKeyword}&api_key=${giphyKey}&limit=24`;
+  const response = await fetch(URL);
   const jsonData = await response.json();
   console.log(jsonData);
   console.log(jsonData.data[0].images.original.url);
   for (let i = 0; i < jsonData.data.length; i++) {
     var giphyImage = document.createElement("img");
     giphyImage.src = jsonData.data[i].images.original.url;
-    giphyImage.height = 100;
-    giphyImage.length = 100;
+    giphyImage.height = 200;
+    giphyImage.width = 200;
     console.log(jsonData.data[i].images.original.url);
     giphyDiv.appendChild(giphyImage);
   }
@@ -39,10 +38,14 @@ searchBtn.addEventListener("click", function handleSearch() {
 // });
 
 // function giphySearch(keyword) {
-//   fetch(
-//     `http://api.giphy.com/v1/gifs/search?q=dogs&api_key=${giphyKey}&limit=5`
-//   )
-//     .then((response) => response.json())
+//   const data = await new Promise((resolve, reject) => {
+//     fetch(
+//         `http://api.giphy.com/v1/gifs/search?q=dogs&api_key=${giphyKey}&limit=5`
+//       )
+//         .then((response) => resolve(response.json()))
+//         .catch(error => reject(error));
+//   })
+
 //     .then((data) => console.log(data));
 // }
 
